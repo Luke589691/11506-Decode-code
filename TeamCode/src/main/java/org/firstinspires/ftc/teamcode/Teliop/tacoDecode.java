27533d24e507107459a8ee7cede149030dacb689
Harry-Schulz-11506 <94622@stpeters.qld.edu.au>
2025-10-21 11:01:40 +1000

    clean up of files (adding folders)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teliop/tacoDecode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teliop/tacoDecode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Teliop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MecanumWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MecanumWheelTest.java
@@ -1,9 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "mecanumTest")
     public class MecanumWheelTest extends LinearOpMode {
